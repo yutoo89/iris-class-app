d6a6529ed0c3f78d2c7b9853eb73de8b5f319ac3
onocci <onoyuta@osiro.it>
2022-04-13 06:46:12 +0900

    lib_versionsライブラリのversion確認のためのファイルを追加

---
+++ src/lib_versions.py
@@ -0,0 +1,20 @@
+import sys
+print('Python version: {}'.format(sys.version))
+
+import pandas as pd
+print('pandas version: {}'.format(pd.__version__))
+
+import matplotlib as mpl
+print('matplotlib version: {}'.format(mpl.__version__))
+
+import numpy as np
+print('numpy version: {}'.format(np.__version__))
+
+import scipy as sp
+print('scipy version: {}'.format(sp.__version__))
+
+import IPython as ip
+print('IPython version: {}'.format(ip.__version__))
+
+import sklearn as skl
+print('scikit-learn version: {}'.format(skl.__version__))
